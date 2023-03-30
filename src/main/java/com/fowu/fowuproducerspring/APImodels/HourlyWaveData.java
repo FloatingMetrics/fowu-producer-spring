a2a500c036399d0e5ff658fad1e0c089fe6e53a7
Therese Stokkan <thestokkan@gmail.com>
2023-03-30 15:49:06 +0200

    Wave data API models

---
+++ src/main/java/com/fowu/fowuproducerspring/APImodels/HourlyWaveData.java
@@ -0,0 +1,12 @@
+package com.fowu.fowuproducerspring.APImodels;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties
+public record HourlyWaveData(
+    @JsonProperty("time") String time,
+    @JsonProperty("wave_height") double waveHeight,
+    @JsonProperty("wave_period") double wavePeriod,
+    @JsonProperty("wave_direction") int waveDirection) {
+}
---
+++ src/main/java/com/fowu/fowuproducerspring/APImodels/WaveDataUnits.java
@@ -0,0 +1,10 @@
+package com.fowu.fowuproducerspring.APImodels;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record WaveDataUnits(
+    @JsonProperty("time") String time,
+    @JsonProperty("wave_height") String waveHeight,
+    @JsonProperty("wave_period") String wavePeriod,
+    @JsonProperty("wave_direction") String waveDirection) {
+}
