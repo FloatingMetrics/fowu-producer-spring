77ffb0926fabe6293593332efaf5d3175f5d30d6
Therese Stokkan <thestokkan@gmail.com>
2023-03-30 16:48:07 +0200

    Created WeatherService for fetching data from APIs

---
+++ src/main/java/com/fowu/fowuproducerspring/WeatherService.java
@@ -0,0 +1,61 @@
+package com.fowu.fowuproducerspring;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.fowu.fowuproducerspring.APImodels.WaveDataAPI;
+import com.fowu.fowuproducerspring.APImodels.MarineDataAPI;
+import com.fowu.fowuproducerspring.APImodels.WeatherDataAPI;
+import com.fowu.fowuproducerspring.APImodels.WindDataAPI;
+
+public class WeatherService {
+
+  RestTemplate RestTemplate;
+  private MarineDataAPI marineData;
+  private WaveDataAPI waveData;
+  private WeatherDataAPI weatherData;
+  private WindDataAPI windData;
+  private final double latitude = 54.3213;
+  private final double longitude = 10.1348;
+
+  public WeatherService(RestTemplate restTemplate) {
+    this.RestTemplate = restTemplate;
+  }
+
+  public WeatherData getWeatherData() {
+
+    waveData = getMarineData();
+    windData = getWindData();
+
+    assert waveData != null && windData != null;
+
+    var weatherData = new WeatherData(waveData.time(), waveData.waveHeight(), waveData.wavePeriod(),
+        waveData.waveDirection(), windData.windDirection(), windData.windSpeed());
+
+    return weatherData;
+  }
+
+  private WaveDataAPI getMarineData() {
+    marineData = RestTemplate.getForObject(
+        "https://marine-api.open-meteo.com/v1/marine?latitude=" + latitude + "&longitude=" + longitude
+            + "&hourly=wave_height,wave_direction,wave_period",
+        MarineDataAPI.class);
+
+    assert marineData != null;
+    waveData = marineData.hourlyData();
+
+    return waveData;
+  }
+
+  private WindDataAPI getWindData() {
+    weatherData = RestTemplate.getForObject(
+        "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude
+            + "&hourly=windspeed_10m,winddirection_10m&windspeed_unit=ms&forecast_days=1",
+        WeatherDataAPI.class);
+
+    assert weatherData != null;
+    windData = weatherData.windData();
+
+    return windData;
+  }
+
+}
