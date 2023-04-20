a28d0a8d1afb57c99514fbc89677ac0862d0794c
Therese Stokkan <thestokkan@gmail.com>
2023-04-20 12:41:35 +0200

    Fix timezone issue

--- strainproducer1/src/main/java/com/fowu/strainproducer/Strain.java
+++ strainproducer1/src/main/java/com/fowu/strainproducer/Strain.java
@@ -1,26 +1,28 @@
 package com.fowu.strainproducer;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Strain implements Serializable {
   String time;
   double strain;
-  LocalDateTime localDateTime = LocalDateTime.now();
-  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+  Instant instant = Instant.now();
+  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").withZone(ZoneId.of("GMT+2"));
 
   public Strain(double strain) {
-    this.time = localDateTime.format(formatter);
+    this.time = formatter.format(instant);
     this.strain = strain;
   }
 
   @Override
   public String toString() {
     return "Strain{" +
-        "time=" + time +
-        ", strain=" + strain +
-        '}';
+           "time=" + time +
+           ", strain=" + strain +
+           '}';
   }
 
 }
