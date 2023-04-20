cea173ec96e810cf756f023938005e23be3f0708
Therese Stokkan <thestokkan@gmail.com>
2023-04-20 12:28:55 +0200

    Added future handling and better logging to send() method

--- weatherproducer/src/main/java/com/fowu/weatherproducer/WeatherController.java
+++ weatherproducer/src/main/java/com/fowu/weatherproducer/WeatherController.java
@@ -2,8 +2,11 @@ package com.fowu.weatherproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class WeatherController {
@@ -44,8 +47,20 @@ public class WeatherController {
   @Scheduled(fixedDelay = 2000)
   public void send() {
     Weather pointData = weatherDataPoints();
-    kafkaTemplate.send(topicName, key, pointData);
-    System.out.println(pointData.toString());
+    CompletableFuture<SendResult<String, Weather>> future = kafkaTemplate.send(topicName, key, pointData);
+    future.whenComplete((result, ex) -> {
+      if (ex == null) {
+        System.out.println("Sent value=[" + result.getProducerRecord().value() +
+                           "] topic=[" + result.getRecordMetadata().topic() +
+                           "] partition=[" + result.getRecordMetadata().partition() +
+                           "] offset=[" + result.getRecordMetadata().offset() +
+                           "]");
+      } else {
+        System.out.println("Unable to send value=[" +
+                           pointData + "] due to : " + ex.getMessage());
+      }
+    });
   }
+
 
 }
