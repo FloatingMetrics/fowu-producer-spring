8d71dc9727ea2c428068d4aa8421b7c74886ee45
Therese Stokkan <thestokkan@gmail.com>
2023-04-20 12:29:09 +0200

    Added future handling and better logging to send() method

--- strainproducer1/src/main/java/com/fowu/strainproducer/StrainController.java
+++ strainproducer1/src/main/java/com/fowu/strainproducer/StrainController.java
@@ -2,13 +2,19 @@ package com.fowu.strainproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class StrainController {
 
   private final StrainService strainService;
+
+  private final String topicName = "strain";
+  private final String key = "str001";
 
   @Autowired
   private KafkaTemplate<String, Strain> kafkaTemplate;
@@ -17,11 +23,22 @@ public class StrainController {
     this.strainService = strainService;
   }
 
-  @Scheduled(fixedDelay = 500)
+  @Scheduled(fixedDelay = 2000)
   public void send() {
     Strain data = strainService.getStrainData();
-    kafkaTemplate.send("strain", "str001", data);
-    System.out.println(data.toString());
+    CompletableFuture<SendResult<String, Strain>> future = kafkaTemplate.send(topicName, key, data);
+    future.whenComplete((result, ex) -> {
+      if (ex == null) {
+        System.out.println("Sent value=[" + result.getProducerRecord().value() +
+                           "] topic=[" + result.getRecordMetadata().topic() +
+                           "] partition=[" + result.getRecordMetadata().partition() +
+                           "] offset=[" + result.getRecordMetadata().offset() +
+                           "]");
+      } else {
+        System.out.println("Unable to send value=[" +
+                           data + "] due to : " + ex.getMessage());
+      }
+    });
   }
 
 }
