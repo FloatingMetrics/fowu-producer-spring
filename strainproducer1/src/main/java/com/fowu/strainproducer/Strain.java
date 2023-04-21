fe7a7c0048278e9b424d6388952f49e511b17608
Therese Stokkan <thestokkan@gmail.com>
2023-04-21 12:43:58 +0200

    Added getters to Strain - strain producer now works

--- strainproducer1/src/main/java/com/fowu/strainproducer/Strain.java
+++ strainproducer1/src/main/java/com/fowu/strainproducer/Strain.java
@@ -2,25 +2,32 @@ package com.fowu.strainproducer;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Strain implements Serializable {
-  String time;
+  String captureTime;
   double strain;
   Instant instant = Instant.now();
   DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").withZone(ZoneId.of("GMT+2"));
 
+  public String getCaptureTime() {
+    return captureTime;
+  }
+
+  public double getStrain() {
+    return strain;
+  }
+
   public Strain(double strain) {
-    this.time = formatter.format(instant);
+    this.captureTime = formatter.format(instant);
     this.strain = strain;
   }
 
   @Override
   public String toString() {
     return "Strain{" +
-           "time=" + time +
+           "captureTime=" + captureTime +
            ", strain=" + strain +
            '}';
   }
