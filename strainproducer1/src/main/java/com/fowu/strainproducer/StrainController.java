b75230aa88de853f1001fbdacc76f6d68a008d8d
Therese Stokkan <thestokkan@gmail.com>
2023-04-21 14:45:36 +0200

    Send strain data to three different partitions

--- strainproducer1/src/main/java/com/fowu/strainproducer/StrainController.java
+++ strainproducer1/src/main/java/com/fowu/strainproducer/StrainController.java
@@ -6,6 +6,7 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Controller
@@ -14,7 +15,14 @@ public class StrainController {
   private final StrainService strainService;
 
   private final String topicName = "strain";
-  private final String key = "str001";
+
+  private final String key1 = "str001";
+  private final String key2 = "str002";
+  private final String key3 = "str003";
+
+  private final int partition1 = 0;
+  private final int partition2 = 1;
+  private final int partition3 = 2;
 
   @Autowired
   private KafkaTemplate<String, Strain> kafkaTemplate;
@@ -25,20 +33,56 @@ public class StrainController {
 
   @Scheduled(fixedDelay = 2000)
   public void send() {
-    Strain data = strainService.getStrainData();
-    CompletableFuture<SendResult<String, Strain>> future = kafkaTemplate.send(topicName, key, data);
-    future.whenComplete((result, ex) -> {
+//    Strain 1
+        Strain data1 = strainService.getStrainData();
+        CompletableFuture<SendResult<String, Strain>> future1 = kafkaTemplate.send(topicName, partition1, key1, data1);
+        future1.whenComplete((result, ex) -> {
+          if (ex == null) {
+            System.out.println("Sent value=[" + result.getProducerRecord().value() +
+                               "] key=[" + result.getProducerRecord().key() +
+                               "] topic=[" + result.getRecordMetadata().topic() +
+                               "] partition=[" + result.getRecordMetadata().partition() +
+                               "] offset=[" + result.getRecordMetadata().offset() +
+                               "]");
+          } else {
+            System.out.println("Unable to send value=[" +
+                               data1 + "] due to : " + ex.getMessage());
+          }
+        });
+
+    //    Strain 2
+    Strain data2 = strainService.getStrainData();
+    CompletableFuture<SendResult<String, Strain>> future2 = kafkaTemplate.send(topicName, partition2, key2, data2);
+    future2.whenComplete((result, ex) -> {
       if (ex == null) {
         System.out.println("Sent value=[" + result.getProducerRecord().value() +
+                           "] key=[" + result.getProducerRecord().key() +
                            "] topic=[" + result.getRecordMetadata().topic() +
                            "] partition=[" + result.getRecordMetadata().partition() +
                            "] offset=[" + result.getRecordMetadata().offset() +
                            "]");
       } else {
         System.out.println("Unable to send value=[" +
-                           data + "] due to : " + ex.getMessage());
+                           data2 + "] due to : " + ex.getMessage());
       }
     });
-  }
 
+    //    Strain 3
+    Strain data3 = strainService.getStrainData();
+    CompletableFuture<SendResult<String, Strain>> future3 = kafkaTemplate.send(topicName, partition3, key3, data3);
+    future3.whenComplete((result, ex) -> {
+      if (ex == null) {
+        System.out.println("Sent value=[" + result.getProducerRecord().value() +
+                           "] key=[" + result.getProducerRecord().key() +
+                           "] topic=[" + result.getRecordMetadata().topic() +
+                           "] partition=[" + result.getRecordMetadata().partition() +
+                           "] offset=[" + result.getRecordMetadata().offset() +
+                           "]");
+      } else {
+        System.out.println("Unable to send value=[" +
+                           data3 + "] due to : " + ex.getMessage());
+      }
+    });
+
+  }
 }
